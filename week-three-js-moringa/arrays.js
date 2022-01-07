3737869dd9b2c29b73d02860d830aea99b992256
moses hanjari <mkubo@safaricom.co.ke>
2022-01-07 11:23:00 +0300

    additions

---
+++ week-three-js-moringa/arrays.js
@@ -0,0 +1,9 @@
+//what is an array
+// []
+
+// method one
+const fruits = ['Mango','Orange','Melon','Banana','Kiwi']
+//Method Two
+const cars= new array('VW','Audi','Toyota','Subaru')
+console.log(fruits)
+console.log(cars)
