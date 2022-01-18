6a43e5ce31bd1246479f2d5c4940e14fe345b6c8
moses hanjari <mkubo@safaricom.co.ke>
2022-01-18 05:23:24 +0300

    new work

--- week-three-js-moringa/arrays.js
+++ week-three-js-moringa/arrays.js
@@ -5,5 +5,19 @@
 const fruits = ['Mango','Orange','Melon','Banana','Kiwi']
 //Method Two
 const cars= new array('VW','Audi','Toyota','Subaru')
+
+//console.log(fruits)
+//console.log(cars)
+
+// Accessing elements within an array
+console.log(fruits[0])
+
+// Add elements to an array
+//Use the PUSH method
+fruits.push('New Addition')
 console.log(fruits)
-console.log(cars)
+
+// Remove elements from an array
+const newVar = fruits.pop();
+console.log(newVar)
+console.log(fruits)
